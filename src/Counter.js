70cab160637f9af05e80956502296a9e956f7b2f
Rahulgard12346 <techierahul8602@gmail.com>
2024-04-06 10:28:44 +0530

    add h1 tag

--- src/Counter.js
+++ src/Counter.js
@@ -6,6 +6,7 @@ const Counter = () => {
 
   return (
     <div> 
+      <h1>This is my first page </h1>
       <button onClick={() => dispatch("Increment")}>Increment +</button>
       <button onClick={() => dispatch("Decrement")}>Decrement -</button>
       <h2>{count}</h2>
